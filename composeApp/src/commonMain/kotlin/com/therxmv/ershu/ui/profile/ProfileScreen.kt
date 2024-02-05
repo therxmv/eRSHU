069150d7e8980f5fc3a005cdfefee5c6db9fcfd6
therxmv <roma.kyzmuch@gmail.com>
2024-02-05 19:04:43 +0200

    [1.0.1] UI/UX improvements

--- composeApp/src/commonMain/kotlin/com/therxmv/ershu/ui/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/therxmv/ershu/ui/profile/ProfileScreen.kt
@@ -1,11 +1,13 @@
 package com.therxmv.ershu.ui.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
@@ -52,101 +55,111 @@ class ProfileScreen : Screen {
                 OfflineBanner()
             }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 24.dp, end = 24.dp, bottom = 12.dp)
-            ) {
-
-                InputTitle(Res.string.profile_specialty)
-                DropDown(
+            if (uiState.yearsList.isNotEmpty()) {
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 12.dp),
-                    isExpanded = isYearExpanded,
-                    input = uiState.selectedYear ?: "",
-                    placeholder = Res.string.profile_choose_year,
-                    onExpandedChange = {
-                        isYearExpanded = it
-                    },
-                    onDismissRequest = {
-                        isYearExpanded = false
-                        focusManager.clearFocus()
-                    },
-                    isEnabled = true,
+                        .fillMaxSize()
+                        .padding(start = 24.dp, end = 24.dp, bottom = 12.dp)
                 ) {
-                    uiState.getYears().forEach {
-                        DropdownMenuItem(
-                            text = {
-                                Text(text = it)
-                            },
-                            onClick = {
-                                viewModel.onEvent(
-                                    ProfileUiEvent.SelectYear(it)
-                                )
-                                isYearExpanded = false
-                            }
-                        )
-                    }
-                }
-                DropDown(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp),
-                    isExpanded = isSpecialtyExpanded,
-                    input = uiState.selectedSpecialty?.specialtyName.orEmpty(),
-                    placeholder = Res.string.profile_choose_specialty,
-                    onExpandedChange = {
-                        isSpecialtyExpanded = it
-                    },
-                    onDismissRequest = {
-                        isSpecialtyExpanded = false
-                        focusManager.clearFocus()
-                    },
-                    isEnabled = uiState.selectedYear != null
-                ) {
-                    uiState.getSpecialties()?.forEach { item ->
-                        val text = item.specialtyName
 
-                        DropdownMenuItem(
-                            text = {
-                                Text(text = text)
-                            },
-                            onClick = {
-                                viewModel.onEvent(
-                                    ProfileUiEvent.SelectSpecialty(item)
-                                )
-                                isSpecialtyExpanded = false
-                            }
-                        )
-                    }
-                }
-
-                Box(modifier = Modifier.fillMaxWidth().weight(1F))
-
-                Text(
-                    modifier = Modifier.padding(vertical = 12.dp),
-                    text = Res.string.profile_continue_label,
-                    style = MaterialTheme.typography.labelLarge,
-                )
-
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = uiState.isFieldsFilled(),
-                    onClick = {
-                        viewModel.onEvent(
-                            ProfileUiEvent.Continue {
-                                navigator.push(
-                                    ScheduleScreen.createScreen(
-                                        uiState.selectedYear,
-                                        uiState.selectedSpecialty?.specialtyName,
+                    InputTitle(Res.string.profile_specialty)
+                    DropDown(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 12.dp),
+                        isExpanded = isYearExpanded,
+                        input = uiState.selectedYear ?: "",
+                        placeholder = Res.string.profile_choose_year,
+                        onExpandedChange = {
+                            isYearExpanded = it
+                        },
+                        onDismissRequest = {
+                            isYearExpanded = false
+                            focusManager.clearFocus()
+                        },
+                        isEnabled = true,
+                    ) {
+                        uiState.getYears().forEach {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(text = it)
+                                },
+                                onClick = {
+                                    viewModel.onEvent(
+                                        ProfileUiEvent.SelectYear(it)
                                     )
-                                )
-                            }
-                        )
-                    },
+                                    isYearExpanded = false
+                                }
+                            )
+                        }
+                    }
+                    DropDown(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
+                        isExpanded = isSpecialtyExpanded,
+                        input = uiState.selectedSpecialty?.specialtyName.orEmpty(),
+                        placeholder = Res.string.profile_choose_specialty,
+                        onExpandedChange = {
+                            isSpecialtyExpanded = it
+                        },
+                        onDismissRequest = {
+                            isSpecialtyExpanded = false
+                            focusManager.clearFocus()
+                        },
+                        isEnabled = uiState.selectedYear != null
+                    ) {
+                        uiState.getSpecialties()?.forEach { item ->
+                            val text = item.specialtyName
+
+                            DropdownMenuItem(
+                                text = {
+                                    Text(text = text)
+                                },
+                                onClick = {
+                                    viewModel.onEvent(
+                                        ProfileUiEvent.SelectSpecialty(item)
+                                    )
+                                    isSpecialtyExpanded = false
+                                }
+                            )
+                        }
+                    }
+
+                    Box(modifier = Modifier.fillMaxWidth().weight(1F))
+
+                    Text(
+                        modifier = Modifier.padding(vertical = 12.dp),
+                        text = Res.string.profile_continue_label,
+                        style = MaterialTheme.typography.labelLarge,
+                    )
+
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        enabled = uiState.isFieldsFilled(),
+                        onClick = {
+                            viewModel.onEvent(
+                                ProfileUiEvent.Continue {
+                                    navigator.push(
+                                        ScheduleScreen.createScreen(
+                                            uiState.selectedYear,
+                                            uiState.selectedSpecialty?.specialtyName,
+                                        )
+                                    )
+                                }
+                            )
+                        },
+                    ) {
+                        Text(text = Res.string.profile_continue)
+                    }
+                }
+            } else {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Text(text = Res.string.profile_continue)
+                    CircularProgressIndicator()
                 }
             }
         }
